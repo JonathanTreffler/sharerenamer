78eb71aa2a111f6207dd5ef88fd0375c19cb4468
DecaTec <jr@decatec.de>
2018-08-30 11:16:06 +0200

    Rename shares in Gallery app; translations

---
+++ js/sharerenamerfiles.js
@@ -0,0 +1,154 @@
+/* global OC, $, t */
+
+ShareRenamerFiles.hijackShare = function () {
+	var ShareDialogLinkShareViewRender = OC.Share.ShareDialogLinkShareView.prototype.render;
+	OC.Share.ShareDialogLinkShareView.prototype.render = function () {
+		var r = ShareDialogLinkShareViewRender.apply(this, arguments);
+		var $linkRenamerButtonElement = this.$el.find('#linkRenamerButton');
+		var $linkText = this.$el.find('.linkText');
+		var $checkBox = this.$el.find('.linkCheckbox');
+
+		if (!$linkRenamerButtonElement.length) {
+			var linktxt = $linkText.val();
+			if (linktxt == "" || linktxt == null || linktxt == false || typeof(linktxt) == 'undefined') {
+				var token = '???';
+			} 
+			else {
+				var n = linktxt.lastIndexOf("/");
+				var token = linktxt.substr(n + 1);
+			}
+			$linkRenamerButtonElement = 
+				'<br>' +
+				'<div id="ShareRenamerDiv">' +
+					'<input id="linkRenamerButton" type="button" class="button" value="' + t('sharerenamer', 'Rename link') + '" />' +
+					'<input id="ShareRenamerNew" type="text" class="hidden" placeholder="" autocomplete="off" spellcheck="false" autocorrect="off" />' +
+					'<br>' +
+					'<input id="ShareRenamerSave" type="button" class="button hidden" value="' + t('sharerenamer', 'Rename') + '" />' +
+					'<input id="ShareRenamerCancel" type="button" class="button hidden" value="' + t('sharerenamer', 'Cancel') + '" />' +
+				'</div><br>';
+			$linkText.after($linkRenamerButtonElement);
+		}
+
+		if ($checkBox.is(':checked')) {
+			$('#ShareRenamerDiv').show();
+		} 
+		else {
+			$('#ShareRenamerDiv').hide();
+		}
+
+		$('#linkRenamerButton').click(function () {			
+			$('#linkRenamerButton').hide();
+
+			var url = $linkText.val();
+			var idx = url.lastIndexOf("/");
+			var baseUrl = url.substr(0, idx + 1);
+
+			var idx2 = url.lastIndexOf("/");
+			var token = url.substr(idx2 + 1);		
+
+			$('#ShareRenamerNew').val('');
+			$('#ShareRenamerNew').attr('placeholder', token);
+			$('#ShareRenamerNew').show();
+			$('#ShareRenamerSave').show();
+			$('#ShareRenamerCancel').show();
+			$('#ShareRenamerNew').focus();
+		});
+
+		$('#ShareRenamerCancel').click(function () {
+			$('#ShareRenamerNew').val('');
+			$('#ShareRenamerSave').click();
+		});
+
+		$('#ShareRenamerNew').keyup(function (event) {
+			if (event.keyCode === 27) {
+				$(this).tooltip('hide');
+				$(this).tooltip('destroy');
+				$('#ShareRenamerCancel').click();
+				return;
+			}
+
+			var rx = /^[a-zA-Z0-9\-_]+$/g;
+			if ($(this).val() != '' && !rx.test($(this).val())) {
+				$(this).tooltip({
+					placement: 'right',
+					trigger: 'manual',
+					title: t('sharerenamer', 'Only the following characters are allowed for links: %s').replace('%s', ' a-z, A-Z, 0-9, -, _ ')
+				});
+				$(this).tooltip('show');
+			} else {
+				$(this).tooltip('hide');
+				$(this).tooltip('destroy');
+
+				if (event.keyCode === 13) {
+					$('#ShareRenamerSave').click();
+					return;
+				}
+			}
+		});
+		
+		$('#ShareRenamerSave').click(function () {
+			$('#shareTabView input').attr('disabled', false);
+			var linktxt = $('.linkText').val();
+			var n = linktxt.lastIndexOf("/");
+			var old_token = linktxt.substr(n + 1);
+			var new_token = $('#ShareRenamerNew').val();
+			var rx = /^[a-zA-Z0-9\-_]+$/g;
+
+			if (new_token == old_token) {
+				$('#ShareRenamerNew').val('');
+			}
+			else if (new_token != '' && !rx.test(new_token)) {
+				// tooltip is shown, so just don't do anything
+				$('#ShareRenamerNew').select();
+				return false;
+			}
+			else if (new_token != '') {
+				var init = new ShareRenamer(OC.generateUrl('/apps/sharerenamer/rename'));
+				var exec = init.Rename(old_token, new_token);
+
+				if (exec == 'pass') {
+					$linkText.val($linkText.val().replace(old_token, new_token));
+					$('#ShareRenamerNew').attr('placeholder', new_token);
+					$('#ShareRenamerNew').val('');
+				} 
+				else if (exec == 'exists') {
+					$('#ShareRenamerNew').tooltip({
+						placement: 'right',
+						trigger: 'manual',
+						title: t('files', 'Link {newname} already exists. Please choose another link name.').replace('{newname}', "'" + new_token + "'")
+					});
+					_.delay(function() {
+						$('#ShareRenamerNew').tooltip('hide');
+						$('#ShareRenamerNew').tooltip('destroy');
+					}, 3000);
+
+					$('#ShareRenamerNew').tooltip('show');
+					$('#ShareRenamerNew').select();
+					return false;
+				} 
+				else if (exec == 'error') {
+					// alert is in AJAX call already
+					return false;
+				}
+			}
+
+			$('#linkRenamerButton').show();
+			$('#ShareRenamerNew').hide();
+			$('#ShareRenamerSave').hide();
+			$('#ShareRenamerCancel').hide();
+		});
+
+		return r;
+	};
+};
+
+$(document).ready(function () {
+		if ($('#body-login').length > 0) {
+			return true; //deactivate on login page
+		}
+
+		if ($('#filesApp').val()) {
+			$('#fileList').one('updated', ShareRenamerFiles.hijackShare);
+		}
+	}
+);
---
+++ l10n/uk.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "sharerenamer",
+    {
+    "Error" : "Помилка",
+    "Rename link" : "Переіменувати посилання",
+    "Rename" : "Переіменувати",
+    "Cancel" : "Відмінити",
+    "Only the following characters are allowed for links: %s" : "Тільки вказані символи дозволені для використання в посиланнях: %s",
+    "Link {newname} already exists. Please choose another link name." : "Посилання {newname} вже існує. Будь-ласка оберіть для посилання інше ім'я.",
+    "ShareRenamer" : "ShareRenamer",
+    "A simple share link renamer add-in for the Files app" : "Доповнення до додатку Files - просте перейменування посилань загальних ресурсів",
+    "This is an add-in to the Files app, which allows you to customize your share tokens, so your links can be https://mycloud.de/s/mysharedlink instead of https://mycloud.de/s/qPv1SwbU5M2YEoJZ. Just share a file or folder (or click an already shared one) and a new button for renaming the link will appear below the 'Copy URL' button in the menu of the share." : "Це доповнення до додатку Files, яке дозволяє Вам налаштовувати вигляд посилань спільних ресурсів, таким чином що Ваше посилання вигляду https://mycloud.de/s/mysharedlink буде виглядати, як https://mycloud.de/s/qPv1SwbU5M2YEoJZ. Просто надайте у спільний доступ файл чи теку (або натисніть на вже наданому у спільний доступ ресурсі) і нова кнопка для перейменування посилання з'явиться нижче кнопки 'Копіювати посилання' ('Copy URL') в меню для ресурсу спільного доступу."
+},
+"nplurals=4; plural=(n % 1 == 0 && n % 10 == 1 && n % 100 != 11 ? 0 : n % 1 == 0 && n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 12 || n % 100 > 14) ? 1 : n % 1 == 0 && (n % 10 ==0 || (n % 10 >=5 && n % 10 <=9) || (n % 100 >=11 && n % 100 <=14 )) ? 2: 3);");
