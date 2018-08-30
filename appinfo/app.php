6ef6208902ba39be9e864aa367810d7d7325fe83
DecaTec <jr@decatec.de>
2018-08-30 11:35:44 +0200

    Rename shares in Gallery app

--- appinfo/app.php
+++ appinfo/app.php
@@ -8,12 +8,23 @@ $app = new Application();
 $c = $app->getContainer();
 $appName = $c->query('AppName');
 
-// Hack which only loads the scripts in the Files app
+// Hack which only loads the scripts in the Files and Gallery app
 $request = $c->query('Request');
 if (isset($request->server['REQUEST_URI'])) {
 	$url = $request->server['REQUEST_URI'];
-	if (preg_match('%apps/files(/.*)?%', $url)) {
-		// add script only when in Files app
+
+	if (preg_match('%apps/files(/.*)?%', $url) || preg_match('%apps/gallery(/.*)?%', $url)) {
+		// Add core script only when in Files and Gallery app
 		Util::addScript($appName, 'sharerenamer');
+	}
+
+	if (preg_match('%apps/files(/.*)?%', $url)) {
+		// Add script only when in Files app
+		Util::addScript($appName, 'sharerenamerfiles');
+	}
+
+	if (preg_match('%apps/gallery(/.*)?%', $url)) {
+		// Add script only when in Gallery app
+		Util::addScript($appName, 'sharerenamergallery');
 	}
 }
