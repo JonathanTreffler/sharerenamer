cb68414c6eeb1d58afb7e4dbab41f61af3363dd9
DecaTec <jr@decatec.de>
2018-09-05 07:46:32 +0200

    Translations

---
+++ l10n/sv.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "sharerenamer",
+    {
+    "Error" : "Fel",
+    "Rename link" : "Byt namn på länk",
+    "Rename" : "Byt namn",
+    "Cancel" : "Avbryt",
+    "Only the following characters are allowed for links: %s" : "Bara följande tecken är tillåtna för länkar: %s",
+    "Link {newname} already exists. Please choose another link name." : "Länk {newname} finns redan. Ange ett annat namn för länken."
+},
+"nplurals=2; plural=(n != 1);");
