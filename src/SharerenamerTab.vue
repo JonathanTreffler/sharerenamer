65e7a56a3651d3e8e09ed40bc86592294ac60e0a
Jonathan Treffler <mail@jonathan-treffler.de>
2021-07-11 14:27:09 +0000

    implemented UI

--- src/SharerenamerTab.vue
+++ src/SharerenamerTab.vue
@@ -1,11 +1,19 @@
 <template>
 	<div>
-		<p>Test</p>
-		<p>{{ fileInfo }}</p>
+		<EmptyContent v-if="shares.length == 0" icon="icon-share">
+			No shares to rename
+			<template #desc>You can create shares in the shares tab</template>
+		</EmptyContent>
+		<AppNavigationItem v-for="share in shares" :key="share.token" :title="share.token" :editable="true" editPlaceholder="token" icon="icon-share" />
 	</div>
 </template>
 
 <script>
+import axios from '@nextcloud/axios';
+
+import AppNavigationItem from '@nextcloud/vue/dist/Components/AppNavigationItem';
+import EmptyContent from '@nextcloud/vue/dist/Components/EmptyContent'
+
 export default {
 	name: 'SharerenamerTab',
 	data() {
@@ -13,7 +21,12 @@ export default {
 			loading: false,
 			token: null,
 			fileInfo: {},
+			shares: [],
 		}
+	},
+	components: {
+		AppNavigationItem,
+		EmptyContent,
 	},
 	computed: {
 		/**
@@ -35,6 +48,9 @@ export default {
 	},
     methods: {
         rename: function(old_token, new_token) {
+
+			console.log(old_token, new_token);
+
             // this._baseUrl already ends with /rename, found in routes.php
             var result = 'error';
             var request = $.ajax({
@@ -56,8 +72,35 @@ export default {
             return result;
         },
 		update(fileInfo) {
+			let self = this;
+
 			console.log(fileInfo);
 			this.fileInfo = fileInfo;
+
+			axios.get("/ocs/v2.php/apps/files_sharing/api/v1/shares", {
+				params: {
+					format: "json",
+					path: this.fileInfo.path + this.fileInfo.name,
+					shared_with_me: false,
+					subfiles: false,
+					reshares: true,
+				}
+			}).then(function(response) {
+				console.log(response);
+				
+				self.shares = [];
+
+				for(let share of response.data.ocs.data) {
+					console.log(share);
+
+					if(share.share_type === 3 && share.can_edit) {
+						self.shares.push({
+							token: share.token,
+							label: share.label,
+						});
+					}
+				}
+			})
 		}
     },
 }
